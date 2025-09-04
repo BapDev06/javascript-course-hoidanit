24e9da34e9fb9146958a18532a6995398b66871a
Ngo Nhat Tuan <ngotuan150706@gmail.com>
2025-09-04 20:41:35 +0700

    update first

--- src/video6/video6.js
+++ src/video6/video6.js
@@ -1,1 +1,7 @@
 console.log("video 6")
+
+var name = 'Bap'
+
+var name1 = "Tuan"; 
+
+console.log(name); 
---
+++ src/video7/video7.js
@@ -0,0 +1,22 @@
+
+//number
+const myAge = 25; 
+const myScore = 9.1; 
+
+console.log("video 18")
+
+console.log(myAge); 
+console.log(myScore); 
+
+//string
+const name = "hoidanit";
+const name1 = 'erc'; 
+const name2 = `javascript`; 
+
+console.log(name)
+
+//boolean: true/false
+const isTuiDepTrai = true; 
+console.log(isTuiDepTrai)
+
+
